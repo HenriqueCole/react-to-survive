c8d0ab096b7f65b18e71cb2e6d003aeeead2b0da
HenriqueCole <henriquecolefernandes@gmail.com>
2022-08-02 21:48:55 -0300

    phase one

--- react-to-survive/src/Pages/Scenary1Page/index.jsx
+++ react-to-survive/src/Pages/Scenary1Page/index.jsx
@@ -1,7 +1,20 @@
+import backgroundImg from "../../assets/Scenaries/scenary1.gif";
+
+import { Link } from "react-router-dom";
+
+import useSound from "use-sound";
+import { useState } from "react";
+
+import { motion } from "framer-motion";
+
+import song from "../../music/game-music.mp3";
+
 export default function Scenary1Page() {
   return (
-    <div>
-      <h1>Scenary 1</h1>
+    <div className="Scenary1PageContainer">
+      <div className="ContainerBackgroundImage">
+        <img className="backgroundImage" src={backgroundImg} alt="" />
+      </div>
     </div>
   );
 }
