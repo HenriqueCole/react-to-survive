c662b745588093f4827d59ae3d391ec0fce01cfc
HenriqueCole <henriquecolefernandes@gmail.com>
2022-08-23 19:44:42 -0300

    update back button

--- src/Pages/RankingPage/index.jsx
+++ src/Pages/RankingPage/index.jsx
@@ -5,15 +5,19 @@ import song from "../../music/game-music.mp3";
 import "./styles.css";
 
 import Rank from "../../components/rank";
+import { useNavigate } from "react-router-dom";
 
 export default function WelcomePage() {
-
+  const navigate = useNavigate();
   return (
     <div className="WelcomePageContainer">
       <body>
         <div className="ContainerBackgroundImage">
           <img className="backgroundImage" src={backgroundImg} alt="" />
-          <div className="containerTextScreen">
+          <div className="containerTextScreenRank">
+            <button className="backButton" onClick={() => navigate("/welcome")}>
+              Voltar
+            </button>
             <div className="containerWelcomeText">
               <h1 className="h1GameName">Ranking</h1>
             </div>
